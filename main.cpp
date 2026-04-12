49c496605c71d9dfbe28a583f99ac6f4933f6a43
Raphael Abrantes <raphael@abrantes.dev>
2026-04-12 11:55:07 +0800

    implot: removed matplot in preference for implot

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 
 #include <iostream>
 #include <portaudio.h>
-#include<matplot/matplot.h>
+#include <GLFW/glfw3.h>
+#include <implot.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include <boost/circular_buffer.hpp>
+#include <vector>
 
 #define SAMPLE_RATE  (44100)
 #define FRAMES_PER_BUFFER (512)
@@ -21,8 +26,8 @@
 typedef int8_t SAMPLE;
 struct paTestData
 {
-    boost::circular_buffer <SAMPLE> left_samples {FRAMES_PER_BUFFER};
-    boost::circular_buffer <SAMPLE> right_samples {FRAMES_PER_BUFFER};
+    boost::circular_buffer <SAMPLE> left_samples {NUM_OF_SAMPLES};
+    boost::circular_buffer <SAMPLE> right_samples {NUM_OF_SAMPLES};
 
 };
 
@@ -57,20 +62,42 @@ static int recordCallback( const void *inputBuffer, void *,
     return paContinue;
 }
 
+void glfw_error_callback(int error, const char* description) { std::cerr << "GLFW Error " << error << ": " << description << std::endl; }
 
 /*******************************************************************/
 int main(){
+    glfwSetErrorCallback(glfw_error_callback);
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(1200, 800, "ImPlot Example", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(0); // Disable vsync
+
+    // Setup context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImPlot::CreateContext();
+
+    // Setup style
+    ImGui::StyleColorsDark();
+
+    // Setup backend
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
 
     try {
-
-        auto x = matplot::linspace(0, FRAMES_PER_BUFFER);
-        auto f = matplot::figure<>(false);
-        f->backend()->run_command("unset warnings");
-        auto aux = f->current_axes();
-        aux->x_axis().visible(false);
-        aux->y_axis().visible(false);
-        aux->xlim({0, FRAMES_PER_BUFFER});
-        aux->ylim({-128, 127});
 
 
         /* From now on, recordedSamples is initialised. */
@@ -99,11 +126,40 @@ int main(){
 
         throw_if_error(Pa_StartStream( stream ));
         std::cout << "\n=== Now recording!! Please speak into the microphone. ===\n" << std::endl;
-        while(Pa_IsStreamActive( stream ) == 1)
+        while(Pa_IsStreamActive( stream ) == 1 && !glfwWindowShouldClose(window))
         {
-            Pa_Sleep(100);
-            aux->plot(x, data.left_samples , "b", x, data.right_samples, "r" );
+            glfwPollEvents();
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+            ImGui::Begin("Test");
+            if (ImPlot::BeginPlot("Test")) {
+                ImPlot::SetupAxisLimits(ImAxis_X1, 0, NUM_OF_SAMPLES);
+                ImPlot::SetupAxisLimits(ImAxis_Y1, -128, 127);
+
+                ImPlot::PlotLine("Left",data.left_samples.linearize(), data.left_samples.size());
+                ImPlot::PlotLine("Right",data.left_samples.linearize(), data.left_samples.size());
+                ImPlot::EndPlot();
+            }
+            ImGui::End();
+            ImGui::Render();
+            int display_w, display_h;
+            glfwGetFramebufferSize(window, &display_w, &display_h);
+            glViewport(0, 0, display_w, display_h);
+            glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            // Swap buffers
+            glfwSwapBuffers(window);
+
         }
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImPlot::DestroyContext();
+        ImGui::DestroyContext();
+        glfwDestroyWindow(window);
+        glfwTerminate();
 
         throw_if_error(Pa_CloseStream( stream ));
 
